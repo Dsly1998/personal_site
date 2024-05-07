882f56128eb6f3f0de74955d686a7202cf27a835
Dallin Sly <krave83@gmail.com>
2024-05-06 23:42:08 -0400

    contact button pulse

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { ReactComponent as Rocket } from "../../images/Rocket.svg";
 import "./Header.css";
 
@@ -14,12 +14,26 @@ function Header({ openModal }) {
   };
 
   const openResume = () => {
-    // Open the Google Drive link in a new tab
     window.open(
       "https://docs.google.com/document/d/1Kik7RVE9IoyRWurswGECNxk805-w4hnN/edit?usp=sharing&ouid=112251850382172380878&rtpof=true&sd=true",
       "_blank"
     );
   };
+
+  useEffect(() => {
+    // Add the pulsing animation every 10 seconds
+    const interval = setInterval(() => {
+      const rocketButton = document.querySelector(
+        ".header__contact-rocket-button"
+      );
+      rocketButton.classList.add("pulse");
+      setTimeout(() => {
+        rocketButton.classList.remove("pulse");
+      }, 700);
+    }, 7000);
+
+    return () => clearInterval(interval);
+  }, []); // Run only once on component mount
 
   return (
     <header className="header">
