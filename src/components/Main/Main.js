89a17b7d6ab5f3ecff3dce3c9717efcb555a5269
Dallin Sly <krave83@gmail.com>
2024-05-08 02:18:38 -0400

    finished product

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -12,68 +12,137 @@ function Main() {
       <h1 className="main__text">Portfolio</h1>
       <div className="main__wrapper">
         <div className="main__card">
-          <button className="main__button">
+          <a
+            href="https://articlelist.ignorelist.com"
+            className="main__button"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <img
               src={newsImage}
               alt="Explore the latest news project"
               className="main__image"
             />
-          </button>
+          </a>
           <div className="main__info">
-            <p className="main__description">News Explorer project</p>
-            <p>Description of the News Explorer project.</p>
+            <h3 className="main__info-title">News Explorer</h3>
+            <p className="main__info-description">
+              News Explorer is a comprehensive project that employs a full-stack
+              approach, leveraging React for the frontend and Node.js for the
+              backend. By integrating a third-party API, this application
+              enables users to register accounts, conduct keyword searches, and
+              save relevant news articles for future reference.
+              https://github.com/Dsly1998/news-explorer-frontend.git
+              https://github.com/Dsly1998/news-explorer-backend.git
+            </p>
           </div>
         </div>
         <div className="main__card main__card-right">
           <div className="main__info">
-            <p className="main__description">Around the US project</p>
-            <p>Description of the Around the US project.</p>
+            <h3 className="main__info-title">Around the USA</h3>
+            <p className="main__info-description">
+              Around the USA is a dynamic front-end project developed using
+              JavaScript, HTML, and CSS. It incorporates a third-party API to
+              introduce a social dimension to the application, allowing users to
+              create profiles and share posts consisting of places and images.
+              Peers can engage with these posts by liking and interacting with
+              other users' content, fostering a vibrant community experience.
+            </p>
           </div>
-          <button className="main__button">
+          <a
+            href="https://github.com/Dsly1998/se_project_aroundtheus.git"
+            className="main__button"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <img
               src={aroundus}
               alt="Explore around the US project"
               className="main__image main__image-us"
             />
-          </button>
+          </a>
         </div>
         <div className="main__card">
-          <button className="main__button">
+          <a
+            href="https://github.com/Dsly1998/se_project_react.git"
+            className="main__button"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <img
               src={ttwtwr}
               alt="Travel the world with TTWTWR project"
               className="main__image"
             />
-          </button>
+          </a>
           <div className="main__info">
-            <p className="main__description">TTWTWR project</p>
-            <p>Description of the Travel the World project.</p>
+            <h3 className="main__info-title">WTWR</h3>
+            <p className="main__info-description">
+              WTWR (What to Wear) is a comprehensive full-stack application
+              developed with React and Node.js. Users can create profiles and
+              save clothing articles within the platform. Leveraging a weather
+              API, the application dynamically tracks and displays suitable
+              clothing options based on the prevailing weather conditions at the
+              user's location. This intuitive feature ensures users are always
+              well-prepared and stylish, regardless of the forecast.
+              https://github.com/Dsly1998/se_project_express.git
+              https://github.com/Dsly1998/se_project_react.git
+            </p>
           </div>
         </div>
         <div className="main__card main__card-right">
           <div className="main__info">
-            <p className="main__description">Coffee Shop project</p>
-            <p>Description of the Coffee Shop project.</p>
+            <h3 className="main__info-title">Coffee Shop</h3>
+            <p className="main__info-description">
+              The Coffee Shop Project is a captivating landing page designed
+              using HTML and CSS specifically for the Practicum cafe. This
+              engaging platform boasts a seamless navigation bar, an enticing
+              menu section, and insightful video tutorials. Additionally,
+              visitors can conveniently make reservations through the website,
+              enhancing their overall experience. With its dynamic and visually
+              appealing elements, the Coffee Shop Project effectively showcases
+              the charm and offerings of the Practicum cafe while providing
+              users with an immersive online experience.
+            </p>
           </div>
-          <button className="main__button">
+          <a
+            href="https://github.com/Dsly1998/se_project_coffeeshop.git"
+            className="main__button"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <img
               src={coffeeshop}
               alt="Discover local coffee shops project"
               className="main__image"
             />
-          </button>
+          </a>
         </div>
         <div className="main__card">
-          <button className="main__button">
+          <a
+            href="https://github.com/Dsly1998/se_project_library.git"
+            className="main__button"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <img
               src={library}
               alt="Browse online library project"
               className="main__image"
             />
-          </button>
+          </a>
           <div className="main__info">
-            <p className="main__description">Online Library project</p>
-            <p>Description of the Online Library project.</p>
+            <h3 className="main__info-title">Practicum Library</h3>
+            <p className="main__info-description">
+              The Practicum Library stands as my inaugural project, crafted with
+              HTML and CSS, meticulously adhering to industry best coding
+              practices. This landing page serves as a comprehensive repository,
+              brimming with invaluable information and insights into the
+              Practicum Library. Offering a seamless user experience, it
+              provides visitors with a wealth of resources and knowledge about
+              the library's offerings, further accentuating its significance as
+              a vital academic resource.
+            </p>
           </div>
         </div>
       </div>
--- src/components/PopupContact/PopupContact.js
+++ src/components/PopupContact/PopupContact.js
@@ -1,19 +1,59 @@
 import React from "react";
 import close from "../../images/close.svg";
+import phone from "../../images/phone.svg";
+import mail from "../../images/mail.svg";
+import github from "../../images/github.svg";
+import linkedin from "../../images/linkedin-square.svg";
 import "./PopupContact.css";
 
 function PopupContact({ onClose }) {
+  const handleOverlayClick = (event) => {
+    // Check if the click is on the modal's direct container to prevent closing when clicking inside the modal content
+    if (event.target.className === "modal") {
+      onClose();
+    }
+  };
+
   return (
-    <div className="modal">
-      <div className="modal__content">
+    <div className="modal" onClick={handleOverlayClick}>
+      <div className="modal__content" onClick={(e) => e.stopPropagation()}>
         <button className="modal__close-button" onClick={onClose}>
-          <img className="modal__close-image" alt="X" src={close} />
+          <img className="modal__close-image" alt="close" src={close} />
         </button>
         <h2 className="modal__title">Contact Information</h2>
         <div className="modal__contact-info">
-          <p>Email: krave83@gmail.com</p>
-          <p>Phone: (415) 377-3261</p>
-          
+          <p className="modal__personals">
+            <img src={mail} alt="Email" className="modal__icon" />
+            Email: krave83@gmail.com
+          </p>
+          <p className="modal__personals">
+            <img src={phone} alt="Phone" className="modal__icon" />
+            Phone: (415) 377-3261
+          </p>
+        </div>
+        <div className="modal__social-links">
+          <a
+            href="https://github.com/Dsly1998"
+            className="modal__link"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="modal__icon-wrapper">
+              <img src={github} alt="GitHub" className="modal__icon" />
+              <p className="modal__icon-label">Git Hub</p>
+            </div>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/dallin-sly-software-engineer"
+            className="modal__link"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="modal__icon-wrapper">
+              <img src={linkedin} alt="LinkedIn" className="modal__icon" />
+              <p className="modal__icon-label">LinkedIn</p>
+            </div>
+          </a>
         </div>
       </div>
     </div>
